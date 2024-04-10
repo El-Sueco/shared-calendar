8ac50bc80997a8afc8baeac20bb88950f1988730
El-Sueco <svenstauder@outlook.at>
2024-04-10 16:10:34 +0200

    init

---
+++ src/main/java/at/home/SharedCalendarApplication.java
@@ -0,0 +1,10 @@
+package at.home;
+
+import io.micronaut.runtime.Micronaut;
+
+public class SharedCalendarApplication {
+
+    public static void main(String[] args) {
+        Micronaut.run(SharedCalendarApplication.class, args);
+    }
+}
